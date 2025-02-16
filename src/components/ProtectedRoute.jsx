57a19088633ba0fa6b51f8d56ae942c38a3c3b7d
Abhishek-hub-coder <sharmaabhishek33721@gmail.com>
2025-02-17 00:23:43 +0530

    2nd commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,13 +3,13 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
-    const user = useSelector((state) => state.auth.userData);  // Adjust this based on your auth slice
+    const user = useSelector((state) => state.auth.userData);
 
     if (!user) {
-        return <Navigate to="/login" replace />;  // Redirect to login if not authenticated
+        return <Navigate to="/login" replace />;
     }
 
-    return children;  // Render the protected component if authenticated
+    return children;
 };
 
 export default ProtectedRoute;
